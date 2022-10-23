c47bed041f0b8aabe4b9c75e33def47dc48288ee
AHTepsilon <alehertoro725@gmail.com>
2022-10-23 15:30:42 -0500

    program can now catch message from user

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import pyperclip as pc
 from pynput.mouse import Controller, Button
 from time import sleep
 
+mouse = Controller()
+
 print("Initiating...")
 
 #Definir la clase del bot
@@ -19,13 +21,54 @@ class DiingoBot:
     #Solo funciona en modo oscuro whatsapp
     def navigation(self):
         try:
-            pos = pt.locateOnScreen("assets/new_message.png", confidence = .7)
+            pos = pt.locateOnScreen("assets/debug.png", confidence = .7) #TODO: change 'debug' to 'new_message'
             pt.moveTo(pos[0:2], duration=self.speed)
             pt.moveRel(-100, 0, duration=self.speed)
             pt.doubleClick(interval=self.click_speed)
         except Exception as e:
-            print('Exception: ', e)
+            print('Exception: (navigation)', e)
+
+    def box_input(self):
+        try:
+            pos = pt.locateOnScreen("assets/file.png", confidence = .7)
+            pt.moveTo(pos[0:2], duration=self.speed)
+            pt.moveRel(100, 15, duration=self.speed)
+            pt.doubleClick(interval=self.click_speed)
+        except Exception as e:
+            print('Exception: (box_input) ', e)
+    
+    def get_message(self):
+        try:
+            pos = pt.locateOnScreen("assets/file.png", confidence = .7)
+            pt.moveTo(pos[0:2], duration=self.speed)
+            pt.moveRel(850, -50, duration=self.speed)
+        except Exception as e:
+            print('Exception: (get_message) ', e)
+
+    def copy_message(self):
+        pt.doubleClick(interval=.3)
+        sleep(self.speed)
+        #pt.moveRel(0, -10, duration=self.speed)
+        #pt.leftClick()
+        #sleep(1)
+
+        #self.message = pc.paste()
+
+        mouse.click(Button.left, 3)
+        sleep(self.speed)
+        mouse.click(Button.right, 1)
+        sleep(self.speed)
+        pt.moveRel(30, -180, duration=self.speed)
+        mouse.click(Button.left, 1)
+        sleep(1)
+
+        self.message = pc.paste()
+        print('Message: ', self.message)
 
 startBot = DiingoBot(speed=.7, click_speed=.7)
-sleep(4)
-startBot.navigation()
+sleep(2)
+#startBot.navigation()
+startBot.get_message()
+sleep(2)
+startBot.copy_message()
+#startBot.box_input()
