0811fda19804bb9dd52191a7b1838b61c6e8c159
AHTepsilon <alehertoro725@gmail.com>
2022-11-15 14:18:03 -0500

    Added some parameters to user class

--- user.py
+++ user.py
@@ -1,9 +1,9 @@
 class User:
     #clase del usuario, info y eso
     def __init__(self):
-       self.name = ""
-       self.level = 0
-       self.Vpoint = 0
+       self._name = ""
+       self._level = 0
+       self._Vpoint = 0
     
     def increasePoints(self):
         self.Vpoint += 1
@@ -19,3 +19,16 @@ class User:
             self.level = 3
         elif self.Vpoint >= 10:
             self.level = 4
+    
+    def get_level(self):
+        return self._level
+      
+    def set_level(self, x):
+        self._level = x
+
+    def get_Vpoint(self):
+        return self._Vpoint
+      
+    def set_Vpoint(self, x):
+        self._Vpoint = x
+  
